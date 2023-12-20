16af6b88c6d9c7d9daeeb8d5a5859cec8d61fb00
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-12-19 20:06:38 -0800

    adding logo1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
-import { logo } from './assets';
-import { Home, CreatePost } from './pages';
+import { logo, logo1 } from './assets';
+import { Home, CreatePost } from './page';
 
 const App = () => {
   return (
     <h1 className="text-3xl font-bold underline">
-     <BrowserRouter>
-     <header>
+      <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <Link to="/">
+        <img src={logo1} alt="logo" className="w-28 object-contain" />
+      </Link>
       
-     </header>
-     </BrowserRouter>
+
+    </header>
+      </BrowserRouter>
     </h1>
   )
 }
--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.svg';
+import logo1 from './logo1.svg';
 import preview from './preview.png';
 
 export {
   download,
   logo,
+  logo1,
   preview,
 };
